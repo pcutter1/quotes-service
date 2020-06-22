d1ceba6c5f5b1e68b7638979f733e13d3ed44030
Paul C <pcutter@cnm.edu>
2020-06-22 15:20:19 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/quotes/service/SourceRepository.java
@@ -0,0 +1,11 @@
+package edu.cnm.deepdive.quotes.service;
+
+import edu.cnm.deepdive.quotes.model.entity.Source;
+import java.util.Iterator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SourceRepository extends JpaRepository<Source, Long> {
+
+  Iterable<Source> getAllByOrderByNameAsc();
+
+}
